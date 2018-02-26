82c75f69a833ee4cb4a05be1da8cfe37cae5c497
jared w <jaredtimothywillis@gmail.com>
2018-02-26 03:40:45 -0800

    CRUD actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220092222) do
+ActiveRecord::Schema.define(version: 20180226093942) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,10 +27,17 @@ ActiveRecord::Schema.define(version: 20180220092222) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index [nil], name: "index_users_on_confirmation_token", unique: true
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table "wikis", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "private"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wikis_on_user_id"
+  end
 
 end
